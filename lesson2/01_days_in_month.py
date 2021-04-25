1c97b9551aef7427d4057f88c82664a5b670b3c7
WebAikido <aleksei.maslukov@yandex.ru>
2021-04-25 14:43:42 +0300

    how many days in what month

--- lesson2/01_days_in_month.py
+++ lesson2/01_days_in_month.py
@@ -7,8 +7,20 @@
 # Если номер месяца некорректен - сообщить об этом
 
 # Номер месяца получать от пользователя следующим образом
-user_input = input("Введите, пожалуйста, номер месяца: ")
-month = int(user_input)
-print('Вы ввели', month)
+def mont():
+    user_input = input("Введите, пожалуйста, номер месяца: ")
+    month_user = int(user_input)
+    print('Вы ввели', month_user)
+    return month_user
 
-# TODO здесь ваш код
+
+month_day = [('January', 31), ('February', 28), ('March', 31), ('April', 30), ('May', 31), ('June', 30), ('July', 31),
+            ('August', 31), ('September', 30), ('October', 31), ('November', 30), ('December', 31)]
+
+month_user = mont()
+if 1 <= month_user <= 12:
+    x = month_user-1
+    print('Вы выбрали месяц', month_day[x][0], 'в нем ', month_day[x][1], 'дней!')
+else:
+    print(month_user, 'Тако месяца не существует выберите месяц от 1 до 12')
+    mont()
