88f29b7b5a25466962280b1ab8f596f14b6d5176
WebAikido <aleksei.maslukov@yandex.ru>
2021-04-26 21:37:29 +0300

    the wall is ready

--- lesson2/07_wall.py
+++ lesson2/07_wall.py
@@ -1,11 +1,30 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
+y1 = 0
+y2 = 50
 
-# TODO здесь ваш код
 
-simple_draw.pause()
+for z in range(7):
+    x1 = -50
+    x2 = 50
+    for x in range(2):
+        for _ in range(7):
+            left_bottom = sd.get_point(x1, y1)
+            right_top = sd.get_point(x2, y2)
+            sd.rectangle(left_bottom=left_bottom, right_top=right_top, color=sd.COLOR_BLUE, width=0)
+            sd.rectangle(left_bottom=left_bottom, right_top=right_top, color=sd.COLOR_RED, width=1)
+            x1 += 100
+            x2 += 100
+        y1 += 50
+        y2 += 50
+        x1 = 0
+        x2 = 100
+    y1 += 0
+    y2 += 0
+
+sd.pause()
