278eb81ecba6510b8d7133b5f97e3f8bf6bca68d
WebAikido <aleksei.maslukov@yandex.ru>
2021-05-07 16:22:41 +0300

    выбор цвета и фигуры для рисования примитивов фигур selection of color and shape for drawing primitive shapes

--- lesson_004/03_shape_select.py
+++ lesson_004/03_shape_select.py
@@ -9,6 +9,62 @@ import simple_draw as sd
 # Код функций из упр lesson_004/02_global_color.py скопировать сюда
 # Результат решения см lesson_004/results/exercise_03_shape_select.jpg
 
-# TODO здесь ваш код
+COLOR_RED = (255, 0, 0)
+COLOR_ORANGE = (255, 127, 0)
+COLOR_YELLOW = (255, 255, 0)
+COLOR_GREEN = (0, 255, 0)
+COLOR_CYAN = (0, 255, 255)
+COLOR_BLUE = (0, 0, 255)
+COLOR_PURPLE = (255, 0, 255)
+
+color = [
+    (COLOR_RED, 'red'),
+    (COLOR_ORANGE, 'orange'),
+    (COLOR_YELLOW, 'yellow'),
+    (COLOR_GREEN, 'green'),
+    (COLOR_CYAN, 'cyan'),
+    (COLOR_BLUE, 'blue'),
+    (COLOR_PURPLE, 'purple'),
+]
+
+a = 1
+
+for i, k in color:
+    print(a, ':', k)
+    a += 1
+print('--------------------------')
+
+
+figures = ['треугольник', 'квадрат', 'пятиугольник', 'шестиугольник']
+a = 1
+for i in figures:
+    print(a, ':', i)
+    a += 1
+def hexagon(point, color, angle=30, length=100):
+    v1 = sd.vector(start=point, angle=angle, length=length, color=color, width=3)
+    return v1
+
+
+def drawing_polygon(number_of_parties, next_point, color):
+    x = int(360 / number_of_parties)
+    for angle in range(0, 361, x):
+        next_point = hexagon(point=next_point, angle=angle, color=color)
+
+
+# pentagon = drawing_polygon(5, next_point=next_point)
+while True:
+    number_color = int(input('Введите желаемый цвет:'))
+    figure_number = int(input('Введите желаемый номер фигуры:'))
+    if 0 < number_color < 8 :
+        color = color[number_color - 1][0]
+        next_point = sd.get_point(300, 300)
+        if 0 < figure_number < 5:
+            figure_number += 2
+            drawing_polygon(number_of_parties=figure_number, next_point=next_point, color=color)
+            break
+        else:
+            print('Вы ввели не коректный номер фигуры')
+    else:
+        print('Вы ввели не коректное число')
 
 sd.pause()
