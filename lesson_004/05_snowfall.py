e115050462380d08174a8881ceed4818ecf98b91
WebAikido <aleksei.maslukov@yandex.ru>
2021-05-16 10:18:11 +0300

    fall of snowflakes to finish падение снежинок доделать

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -14,18 +14,36 @@ N = 20
 # sd.get_point()
 # sd.snowflake()
 # sd.sleep()
-# sd.random_number()
+sd.random_number()
 # sd.user_want_exit()
 
 # TODO здесь ваш код
+snowflake_data = [[120, 500], [40, 500], [270, 500], [100, 500], [140, 500], [290, 500], [430, 500], [370, 500]]
+
+
+def drawing_snowflakes(x, y):
+    point = sd.get_point(x, y)
+    sd.snowflake(center=point, length=lenght)
+
+
+
+
+lenght = sd.random_number(10, 40)
+
 while True:
     sd.clear_screen()
-    pass
-    pass
-    pass
+    for x, y in snowflake_data:
+        drawing_snowflakes(x=x, y=y)
+    for i in snowflake_data:
+        i[1] -= 10
+        if i[1] < 50:
+            break
+        i[0] += 10
     sd.sleep(0.1)
     if sd.user_want_exit():
         break
+
+
 
 sd.pause()
 
